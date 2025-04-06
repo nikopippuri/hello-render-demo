cade5a640bfd326c409ad4425f7edd0bfa64b6c6
Niko Pippuri <niko.pippuri@gmail.com>
2025-04-06 20:09:05 +0300

    update test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello world from Node! Where is Render?');
+  res.send('Im right here!');
 });
 
 app.listen(PORT, () => {
